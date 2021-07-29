39799eaaa0cbb0770c71ce856ea3fe45069f6ae0
shawn-1997 <2512193x@student.gla.ac.uk>
2021-07-29 22:18:40 +0800

    chapter 7

--- rango/views.py
+++ rango/views.py
@@ -2,16 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
+from django.shortcuts import redirect
+from django.urls import reverse
+from rango.forms import PageForm
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    # Spoiler: you don't need to pass a context dictionary here.
     return render(request, 'rango/about.html')
 
 def show_category(request, category_name_slug):
@@ -27,3 +33,17 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
 
     return render(request, 'rango/category.html', context=context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('/rango/')
+        else:
+            print(form.errors)
+    
+    return render(request, 'rango/add_category.html', {'form': form})
